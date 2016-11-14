35d4fcb4d292bad7225a45b84e5357efdfea4cc4
liutaotao <liutaotao@aixuedai.com>
2016-11-14 20:37:49 +0800

    a

---
+++ com.learn/src/main/java/myProject/com/learn/App.java
@@ -0,0 +1,13 @@
+package myProject.com.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
