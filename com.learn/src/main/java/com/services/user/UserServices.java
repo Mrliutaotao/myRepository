29f654b4076438bf0de05f55766c0320d83c456d
liutaotao <liutaotao@aixuedai.com>
2016-11-15 11:39:22 +0800

    addd

---
+++ com.learn/src/main/java/com/services/user/UserServices.java
@@ -0,0 +1,11 @@
+package com.services.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServices {
+	
+	public void testServices(){
+		System.out.println("servicessss");
+	}
+}
